dfb35b80ee7442c13d9b37594f0370962e40d5a7
mohit12-dev <mohitsapkota678@gmail.com>
2025-11-23 13:32:10 +0545

    async js

---
+++ index.js
@@ -0,0 +1,41 @@
+// console.log("hello start");
+// setTimeout(()=> {
+//     console.log("This prints after 2 seconds");
+// },2000);
+// console.log("End");
+
+
+
+// let num = 1;
+// setTimeout(() => {
+//     num = 5;
+// },10);
+// setTimeout(() => {
+//     console.log("A:",num);
+// },0);
+// console.log("B:",num);
+
+
+// console.log("Step 1");
+// setTimeout(() => {
+//     console.log("step 2 ")
+// },0);
+// setTimeout(() =>{
+//     console.log("step 3")
+// },500);
+// setTimeout(() => {
+//     console.log("step4")
+// },0);
+
+
+
+console.log("A");
+setTimeout(() =>{
+    console.log("B")
+},5);
+setTimeout(() =>{
+    console.log("C")
+},5);
+setTimeout(()=>{
+    console.log("D");
+},50);
